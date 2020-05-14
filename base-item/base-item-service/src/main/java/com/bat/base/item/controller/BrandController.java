53097317defe82a2d6897cd65c28ef3b27979829
zhdf <1308875885@qq.com>
2020-05-14 15:26:25 +0800

    冒泡排序

--- base-item/base-item-service/src/main/java/com/bat/base/item/controller/BrandController.java
+++ base-item/base-item-service/src/main/java/com/bat/base/item/controller/BrandController.java
@@ -27,4 +27,55 @@ public class BrandController {
         }
         return ResponseEntity.ok(brandList);
     }
+
+    public void bubbleSort(int[] arr){
+        System.out.println("冒泡排序");
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=i; j<arr.length-i-1; j++){
+                if(arr[j]>arr[j+1]){
+                    arr[j]+=arr[j+1];
+                    arr[j+1] = arr[j]-arr[j+1];
+                    arr[j]-=arr[j+1];
+                }
+            }
+        }
+    }
+
+    /**
+     * 终极版冒泡排序
+     * 加入一个布尔变量,如果内循环没有交换值,说明已经排序完成,提前终止
+     * @param arr
+     */
+    public static void sortPlus(int[] arr){
+        if(arr != null && arr.length > 1){
+            for(int i = 0; i < arr.length - 1; i++){
+                // 初始化一个布尔值
+                boolean flag = true;
+                for(int j = 0; j < arr.length - i - 1 ; j++){
+                    if(arr[j] > arr[j+1]){
+                        // 调换
+                        int temp;
+                        temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                        // 改变flag
+                        flag = false;
+                    }
+                }
+                if(flag){
+                    break;
+                }
+            }
+        }
+    }
+
+    public void doNothing(){
+        System.out.println("Monday");
+        System.out.println("周二");
+        System.out.println("周三");
+        System.out.println("Thursday");
+        System.out.println("Firday");
+    }
+
+
 }
